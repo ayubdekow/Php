76aec14dc804769441c682504e6961139639a39f
ayubdekow <145904872+ayubdekow@users.noreply.github.com>
2024-01-14 22:24:15 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+    include('functions.php');
+   
+   session_start();
+   checkLoginStatus();
+   
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title><!DOCTYPE html>
+   <link rel="stylesheets" href="index.css">
+</head>
+<link rel="stylesheet" href="index.css">
+<body>
+    <h1><u>WELCOME TO COCCO RESTAURANT</u></h1>
+    <div class="container">
+      <div class="header">
+        <nav>
+            <a href="signup.php">SIGN UP</a>
+        </nav>
+      </div>
+       <div class="section">
+        <h2><u>Delicious foods and drinks that will give you goosebumos!</u></h2>
+         <p>Cocco is a new and innovative and interesting restaurant that is quickly gaining attraction in the hotel industry. Known for its delicious and inexpensive gameplay,Cocco is the perfect choice for all levels of the people
+        </p>
+        <p> 
+            At Cocco we offer the following ;
+        </p>
+        <ul>
+            <li>Wedding halls </li>
+            <li>Pleasing lodges</li>
+            <li>Delicious meals</li>
+            <li>Good services of payment and transaction</li>
+            <li>Wide range of recipes</li>
+            <li>Honeymoon rooms</li>
+        </ul>
+        </div>
+       <div class="footer">
+        <p>
+            Waiting to get started? Well its simple and easy. Simply create an account by signing up on the link below to enjoy our services!
+            <a href="signup.html">SIGN UP</a>. you can as well contact our  customer and hospitality service or email us. Thank you and wlcome to Cocco.
+        </p>
+        <br>
+        <p>
+            Copyright &copy; Cocco Restaurant. All rights reserved
+        </p>
+        <ul>
+            <li><u>GMAIL</u></li>
+            <ol>
+                coccorestaurant@gmail.com
+            </ol>
+            <li>CELL</li>
+            <ol>
+                +276879789877
+            </ol>
+        </ul>
+       </div>
+   </div> 
+    
+</body>
+</html>
